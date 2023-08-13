a07a4e8ec4ca9957de0c0184ae7f17b085601f48
Nakshatra Jain <nakshatrajain246@gmail.com>
2023-08-14 03:02:24 +0530

    Added follow us

--- src/app/follow-us/follow-us.component.ts
+++ src/app/follow-us/follow-us.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./follow-us.component.css']
 })
 export class FollowUsComponent {
-
+  links=[
+    {
+      id:'fb',
+      name:'Facebook',
+      imageLink:'assets/images/social-media/facebook.png',
+      link:''
+    },
+    {
+      id:'insta',
+      name:'Instagram',
+      imageLink:'assets/images/social-media/instagram.png',
+      link:''
+    },
+    {
+      id:'tweet',
+      name:'Twitter',
+      imageLink:'assets/images/social-media/twitter.png',
+      link:''
+    },
+  ]
 }
