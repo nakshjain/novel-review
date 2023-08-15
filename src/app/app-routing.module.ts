ff7ca95c5547c57e510d11e4c61c5cf2d516521a
Nakshatra Jain <nakshatrajain246@gmail.com>
2023-08-15 13:06:33 +0530

    Fixed Auto Scroll

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'top'
+    }),
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
